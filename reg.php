2af26ec59afb4b7e19f6badec8a7f7726bcb3008
park w <demoparking2018@gmail.com>
2017-04-23 10:09:59 -0500

    doneeee

--- reg.php
+++ reg.php
@@ -4,7 +4,7 @@ $name=$_REQUEST['txtFullname'];
 $email=$_REQUEST['txtemail'];
 $phone=$_REQUEST['txtphone'];
 $rfid=$_REQUEST['txtrfid'];
-$password=$_REQUEST['txtpassword'];
+$Password=$_REQUEST['txtpassword'];
 
 //connect to mysql
 $server="us-cdbr-iron-east-03.cleardb.net";
@@ -21,7 +21,7 @@ else
 {
     mysql_select_db("ad_254e48c6f6af81f");
     
-    $query="insert into reg values('$name','$email','$phone','$rfid','$password')";
+    $query="insert into reg values('$name','$email','$phone','$rfid','$Password')";
     
     $result=mysql_query($query,$connection);
     
