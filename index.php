337291f897a2a7c7982145dadfb52e6b0230f3ac
park w <demoparking2018@gmail.com>
2017-04-23 09:33:58 -0500

    comitt

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
+	<title>welcome</title>
 </head>
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+	<center>
+	<fieldset>
+	<h3>Click Register to register<h3>
+<a href="/reg.html">Register</a></br>
+
+  	</fieldset>
+  </center>
+	</body></html>
---
+++ reg.php
@@ -0,0 +1,40 @@
+<?php
+
+$name=$_REQUEST['txtFullname'];
+$email=$_REQUEST['txtemail'];
+$phone=$_REQUEST['txtphone'];
+$rfid=$_REQUEST['txtrfid'];
+$password=$_REQUEST['txtpassword'];
+
+//connect to mysql
+$server="us-cdbr-iron-east-03.cleardb.net";
+$username="bb8ff899f74f6a";
+$password="ff6ca091";
+
+$connection=mysql_connect($server,$username,$password);
+
+if(!$connection)
+{
+    echo "connection failed";
+}
+else
+{
+    mysql_select_db("ad_254e48c6f6af81f");
+    
+    $query="insert into reg values('$name','$email','$phone','$rfid','$password')";
+    
+    $result=mysql_query($query,$connection);
+    
+    if(!$result)
+    {
+        echo "insertion failed";
+    }
+    else
+    {
+        echo "inserted successfully";
+    }
+    mysql_close($connection);
+}
+
+
+?>
