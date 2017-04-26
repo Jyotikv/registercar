039c7d795940192a351e3a4e77723ee52886921c
park w <demoparking2018@gmail.com>
2017-04-26 11:46:27 +0000

    jy

--- reg.php
+++ reg.php
@@ -29,30 +29,7 @@ else
     }
     else
     {
-    	 mysql_select_db("ad_254e48c6f6af81f");
-     $sql2="Select Username from regg where Username='jyoti'";	
-   	$ret_val=mysql_query($sql2, $connection) or die ("Error".mysql_error());
-    if($ret_val)
-    {
-    	if(mysql_num_rows($ret_val)>0)
-    	{
-    		echo "<html><center>";
-    	while($row=mysql_fetch_array($ret_val, MYSQL_NUM))	
-		echo ($row[0]."</br>");
-		echo ("already exits");	
-		echo "</center></html>";
     	
-    	}
-	else
-	{
-		echo "no field"
-	} 
-}
-
-    else
-	{
-	echo (mysql_error()."Error");	
-  }
 
       echo "inserted successfully";
     }
