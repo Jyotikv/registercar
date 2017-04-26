9baacb1af5233f713e598c03b35ec237d476436d
park w <demoparking2018@gmail.com>
2017-04-26 09:55:03 +0000

    njkngkd

--- fetch.php
+++ fetch.php
@@ -1,9 +1,11 @@
 
  <?php
+ 
+ 	//$name=$_REQUEST['txtFullname'];
 	$con=mysql_connect('us-cdbr-iron-east-03.cleardb.net','bb8ff899f74f6a','ff6ca091')  or die ("Con Error".mysql_error());
     mysql_select_db('ad_254e48c6f6af81f',$con);
           
-    $sql="Select *from regg";	
+    $sql="Select Username from regg where Username='Jyoti'";	
    
     $ret_val=mysql_query($sql, $con) or die ("Error".mysql_error());
     if($ret_val)
