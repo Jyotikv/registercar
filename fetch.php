05d116419e555c72b444b09e3277db63ff12036c
park w <demoparking2018@gmail.com>
2017-05-03 17:29:03 +0000

    kll

--- fetch.php
+++ fetch.php
@@ -1,5 +1,5 @@
 <?php
-$car= $_REQUEST['txtcar2']
+$car= $_REQUEST['txtcar2'];
 $con=mysql_connect("us-cdbr-iron-east-03.cleardb.net","bb8ff899f74f6a","ff6ca091") or die (mysql_error());
 // Check connection
 mysql_select_db("ad_254e48c6f6af81f") or die (mysql_connect_errno());
