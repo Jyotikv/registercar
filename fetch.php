9d4ae405bf3ddf75a1e6d675818790edaf0e3d6b
park w <demoparking2018@gmail.com>
2017-05-03 17:23:28 +0000

    klkl

--- fetch.php
+++ fetch.php
@@ -4,7 +4,7 @@ $con=mysql_connect("us-cdbr-iron-east-03.cleardb.net","bb8ff899f74f6a","ff6ca091
 // Check connection
 mysql_select_db("ad_254e48c6f6af81f") or die (mysql_connect_errno());
 
-$strsql="SELECT * FROM demo WHERE carno='$car';
+$strsql="SELECT * FROM demo WHERE carNo='$car';
 $rs=mysql_query($strsql);
   while ($row=mysql_fetch_array($rs))
     {
