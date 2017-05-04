e4b2856cc27e0d76c7b353e9c6eba2eccf6fc573
park w <demoparking2018@gmail.com>
2017-05-04 19:06:25 +0000

    lkmjhng

--- index.php
+++ index.php
@@ -6,9 +6,12 @@
 <body>
 	<center>
 	<fieldset>
-	<h3>Click Register to register<h3>
-<a href="/reg.html">Register</a></br>
-
-  	</fieldset>
+	<h3>Car parking system <h3>
+<a href="/login.html"><h3>Login</h3></a></br>
+<a href="/reg.html"><h3>Register</h3></a></br>
+<a href="/fetch.html"><h3>fetch</h3></a></br>
+<a href="/reserv.html"><h3>Reserv</h3></a></br>
+<a href="/delete.html"><h3>Delete</h3></a></br>
+    	</fieldset>
   </center>
 	</body></html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+$Uname= $_REQUEST['txtname1'];
+$pass1=$_REQUEST['txtpass1'];
+$con=mysql_connect("us-cdbr-iron-east-03.cleardb.net","bb8ff899f74f6a","ff6ca091") or die (mysql_error());
+// Check connection
+mysql_select_db("ad_254e48c6f6af81f") or die (mysql_connect_errno());
+
+$strsql="SELECT * FROM regg WHERE Username='Uname' and password='$pass1'";
+$rs=mysql_query($strsql,$con);
+if($rs)
+ $cnt=mysql_affected_rows($con);
+ if($cnt)
+{
+  while ($row=mysql_fetch_array($rs))
+    {
+   // echo "<dt>Car Number:</dt><dd>".$row["0"]."</br>" ;
+    //echo "<dt>Date:</dt><dd>".$row["1"]."</br>";
+    //echo "<dt>Duration:</dt><dd>".$row["2"]."</br>";
+    echo "you are succefully loged in"
+    }
+
+}
+else
+{
+	echo "please register";
+}
+mysql_close($con);
+?>
