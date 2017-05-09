99e5eacbb52bd67913db32aca93e758416361945
park w <demoparking2018@gmail.com>
2017-05-09 19:08:24 +0000

    sdfg

--- allocation.php
+++ allocation.php
@@ -19,22 +19,22 @@ if(!$connection)
 else
 {
     mysql_select_db("ad_254e48c6f6af81f");
-    $strsql="SELECT * FROM demo WHERE crtDate='$date1' and Fromtime='$time1' and Todate='$time2' ";
+    $strsql="SELECT * FROM demo WHERE crtDate='$date1' and Fromtime='$time1' and Totime='$time2' ";
  	$rs=mysql_query($strsql,$connection);
 	if($rs)
  	$cnt=mysql_affected_rows($connection);
- if($cnt)
-{
+ 	if($cnt)
+	{
   while ($row=mysql_fetch_array($rs))
     {
     echo "Reserved slots :".$row["5"] ;
     
     }
-    if($row[5]==1)
+    if($row["5"]==1)
     {
     	echo "remaining slots are 2,3,4";
     }
-    else if($row[5]==2)
+    else if($row["5"]==2)
     {
     	echo "remaining slots are 1,3,4";
     }
