901065f6df78560487000743b7b4db382fc7df77
park w <demoparking2018@gmail.com>
2017-05-11 13:05:00 -0500

    tyyu

--- delete.php
+++ delete.php
@@ -8,9 +8,12 @@ $con = mysql_connect($host,$uname,$pwd) or 	die("connection failed");
 mysql_select_db($db,$con) or die("db selection failed"); 
    	session_start();
 
-	$rfid = $_REQUEST['txtcar4'];
+	$car5= $_REQUEST['txtcar4'];
+	$date5 = $_REQUEST['txtdate4'];
+	$entry5 = $_REQUEST['txtentry4'];
+	$exit5 = $_REQUEST['txtexit4'];
 	
- if($result=mysql_query("delete from demo where carNo='$rfid'",$con))
+ if($result=mysql_query("delete from demo where carNo='$car5' and crtTime='$date5' and Entrytime='$entry5' and Exittime='$exit5'",$con))
 $cnt=mysql_affected_rows($con);
 if(!$cnt)
 {
