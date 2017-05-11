8bcb5da40c64652d44e09e58c8cb87badb6d1f53
park w <demoparking2018@gmail.com>
2017-05-11 10:09:38 -0500

    dfdf

--- reserve2.php
+++ reserve2.php
@@ -48,27 +48,27 @@ else
   	}
     else
     {
-        echo "Reserved succefully";
+        echo "Reserved succefully .";
     }
      if($duration==1)
 {
 	$res1=$duration*50;
-	echo "your amount  is".$res1;
+	echo "Your amount  is".$res1;
 }
 else if($duration==2)
 {
 	$res2=$duration*50;
-	echo "your amount is".$res2;
+	echo "Your amount is".$res2;
 }
 else if($duration==3)
 {
 	$res3=$duration*50;
-	echo "your amount is".$res3;
+	echo "Your amount is".$res3;
 }
 else if($duration==4)
 {
 	$res4=$duration*50;
-	echo "your amount is".$res4;
+	echo "Your amount is".$res4;
 }
 else
 {
