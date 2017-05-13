1ada3ed9810471cdf108a51f7a11b9efa256c9b8
park w <demoparking2018@gmail.com>
2017-05-13 05:21:06 -0500

    mko

--- reserve2.php
+++ reserve2.php
@@ -185,10 +185,52 @@ else
 	}
 	else
 	{
-		echo "check the detalis";
+		{
+		$duration=$resexit-$resentry;
+		if($resentry<$resexit && $duration<=4)
+		{
+    	$query="insert into demo values('$carno','$resdate','$resentry','$resexit','$resslot')";
+       $result=mysql_query($query,$connection);
+	 	 if(!$result)
+    {
+       echo "Reservation failed";
+  	}
+    else
+    {
+        echo "Reserved succefully .";
+    }
+     if($duration==1)
+{
+	$res1=$duration*50;
+	echo "Your amount  is ".$res1;
+}
+else if($duration==2)
+{
+	$res2=$duration*50;
+	echo "Your amount is ".$res2;
+}
+else if($duration==3)
+{
+	$res3=$duration*50;
+	echo "Your amount is ".$res3;
+}
+else if($duration==4)
+{
+	$res4=$duration*50;
+	echo "Your amount is ".$res4;
+}
+else
+{
+	echo "Duration must be below 4 hours";
+}
+	}
+	else
+	{
+		echo "Entry time must be less than exit time and duration must be below 4 hours";
 	}
 }
-}
+	}
+}}
 }
 mysql_close($connection);
 ?>
